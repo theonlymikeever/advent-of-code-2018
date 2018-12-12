0498a011f4903257aa5351f193ce2ab15bfa2e18
Mike Kerslake <theonlymikeever@gmail.com>
2018-12-12 10:59:34 -0500

    Day 6 - Part 1

--- day5-alchemical-reduction/part2.js
+++ day5-alchemical-reduction/part2.js
@@ -6,33 +6,7 @@ const removeUnit = (letter, data) => {
 };
 
 const alphaMap = {
-  a: '',
-  b: '',
-  c: '',
-  d: '',
-  e: '',
-  f: '',
-  g: '',
-  h: '',
-  i: '',
-  j: '',
-  k: '',
-  l: '',
-  m: '',
-  n: '',
-  o: '',
-  p: '',
-  q: '',
-  r: '',
-  s: '',
-  t: '',
-  u: '',
-  v: '',
-  w: '',
-  x: '',
-  y: '',
-  z: ''
-};
+  a: '', b: '', c: '', d: '', e: '', f: '', g: '', h: '', i: '', j: '', k: '', l: '', m: '', n: '', o: '', p: '', q: '', r: '', s: '', t: '', u: '', v: '', w: '', x: '', y: '', z: ''};
 
 const removeLetters = () => {
   for (let letter in alphaMap) {
@@ -47,7 +21,7 @@ const reduceOnMap = () => {
     alphaMap[letter] = reduced.size;
   }  
 };
-
+ 
 removeLetters();
 reduceOnMap();
 console.log(alphaMap)
