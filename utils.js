5128163e378496b5e127765f12c706a02df5d4ae
Mike Kerslake <theonlymikeever@gmail.com>
2019-01-14 14:23:28 -0500

    Update split util to accept formatting fnc

--- utils.js
+++ utils.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const splitData = (data, delimiter = '\n') => data.split(delimiter);
+const splitData = (data, {delimiter = '\n', format}) => {
+  // format will be a function that gets mapped to transform the entries
+  if (format) return data.split(delimiter).map(format);
+  return data.split(delimiter);
+};
 
 const readData = (fileName, options = {}) => {
   let data = fs.readFileSync(path.resolve(__dirname, fileName), 'utf8');
   data = data.trim(); // why these files have whitespace is beyond me
   if (options.split) {
-    return splitData(data, options.delimiter);
+    return splitData(data, options);
   }
   return data;
 };
