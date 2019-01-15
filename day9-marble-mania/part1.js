66b69539731446c02ebb0d92019ee566c9621685
Mike Kerslake <theonlymikeever@gmail.com>
2019-01-15 10:00:37 -0500

    Day 9 - Part 1 Complete

--- day9-marble-mania/part1.js
+++ day9-marble-mania/part1.js
@@ -1,6 +1,9 @@
+// This solutions works, but it's incredibly slow with massive data sets. Will come back to optimize.
+
 const { testData, puzzleData } = require('./input');
 
-const test = [0];
+// https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers/17323608#17323608
+const mod = (n, m) => ((n % m) + m) % m;
 
 const placeMarble = (currentMarble, circle, marble) => {
   let newCircle;
@@ -8,18 +11,20 @@ const placeMarble = (currentMarble, circle, marble) => {
   if (marble % 23 === 0) {
     // the marble 7 marbles counter-clockwise from the current marble is removed
     // marble located immediately clockwise of the marble that was removed becomes the new current marble
-    let removalPoint = currentMarble - 7;
+    let removalPoint = mod(currentMarble - 7, circle.length);
     let removed = circle[removalPoint];
-    console.log('adding', marble, '&', removed)
     let score = marble + removed;
-    newCircle = [...circle.slice(0, removalPoint), ...circle.slice(removalPoint + 1)];
+    newCircle = [
+      ...circle.slice(0, removalPoint),
+      ...circle.slice(removalPoint + 1)
+    ];
     currentMarble = removalPoint;
-    console.log('** Point scored!', score);
-    console.log('current marble', currentMarble)
     return { newCircle, currentMarble, score };
   }
-  // do stuff
-  if (circle.length === 1) return {newCircle: [...circle, marble], currentMarble: 1};
+
+  // first placement
+  if (circle.length === 1)
+    return { newCircle: [circle[0], marble], currentMarble: 1 };
   /* 
     marble is placed clockwise:
       - to the right of currentMarble if currentMarble idx < circle.length
@@ -29,11 +34,15 @@ const placeMarble = (currentMarble, circle, marble) => {
   if (currentMarble === circle.length - 1) {
     // going full circle! place in first slot
     newCircle = [circle[0], marble, ...circle.slice(1)];
+    currentMarble = 1;
   } else {
-    newCircle = [...circle.slice(0, currentMarble + 2), marble, ...circle.slice(currentMarble + 2)];
+    newCircle = [
+      ...circle.slice(0, currentMarble + 2),
+      marble,
+      ...circle.slice(currentMarble + 2)
+    ];
+    currentMarble = newCircle.indexOf(marble);
   }
-
-  currentMarble = newCircle.indexOf(marble);
 
   return {
     currentMarble,
@@ -46,12 +55,17 @@ const playMarbles = (playerCount, maxPoints) => {
   let marble = 0;
   let circle = [marble];
   let currentMarble = marble;
-
+  let round = 1;
   while (marble < maxPoints) {
-    // console.log('new round')
-    for (let playerNum = 1; playerNum <= playerCount && marble < maxPoints; playerNum++) {
+    round++; // purely for console output
+    console.log('New round:', round, 'Current marble:', marble);
+    for (
+      let playerNum = 1;
+      playerNum <= playerCount && marble < maxPoints;
+      playerNum++
+    ) {
       marble++; // increase marble size
-      console.log('player', playerNum, 'placing marble', marble);
+      // console.log('player', playerNum, 'placing marble', marble);
       let result = placeMarble(currentMarble, circle, marble);
       if (result.score) {
         scoreMap[playerNum] = scoreMap[playerNum] || 0;
@@ -60,8 +74,9 @@ const playMarbles = (playerCount, maxPoints) => {
       circle = result.newCircle;
       currentMarble = result.currentMarble;
     }
+    // console.log(circle);
   }
-  console.log(circle)
+  
   return {
     circle,
     scoreMap
@@ -70,9 +85,12 @@ const playMarbles = (playerCount, maxPoints) => {
 
 const findHighestScore = scoreMap => {
   let scores = Object.keys(scoreMap).map(player => scoreMap[player]);
-  return scores.sort((a, b) => b - a);
+  return scores.sort((a, b) => b - a)[0];
 };
-let {playerCount, maxPoints} = testData[1];
+
+// let { playerCount, maxPoints } = testData[5];
+let { playerCount, maxPoints } = puzzleData;
 const results = playMarbles(playerCount, maxPoints);
-console.log(results.scoreMap)
-console.log('highest', findHighestScore(results.scoreMap)[0]);
+
+console.log(results.scoreMap);
+console.log('highest', findHighestScore(results.scoreMap));
