b4e4fc5c97f1aa28ee5287941cb088102e05aeaa
Mike Kerslake <theonlymikeever@gmail.com>
2019-01-17 10:59:04 -0500

    Day 10 - Optimized!

--- day10-the-stars-align/part1.js
+++ day10-the-stars-align/part1.js
@@ -1,14 +1,67 @@
 /* 
-  create a map of each tick
-  run through the map
-  find grid size
-  search for height change
-  find perfect second
-  build and fill grid
-  flatten and print map
+  This solution actually solves both parts of Day 10!s
+
+  Process:
+    - create an instruction set of each tick (applying velocity to position)
+    - analyze whether the height continues to shrink
+    - find when height reverts - this is the the moment right after constellation
+    - use those instructions to build and fill grid
+    - flatten and print grid to display message
  */
 
 const {data, testData} = require('./input');
+
+const ticker = data => {
+  return data.map(({position, velocity}) => {
+    let x = position[0] + velocity[0];
+    let y = position[1] + velocity[1];
+    return {
+      position: [x, y],
+      velocity
+    };
+  });
+};
+
+const findConstellation = data => {
+  let found = false;
+  let height = Infinity;
+  let second = 1;
+  let tick = data;
+  const previousTicks = [];
+  
+  while (!found) {
+    tick = ticker(tick);
+    const yPositions = tick.map(({position}) => position[1]);
+    const min = Math.min(...yPositions);
+    const max = Math.max(...yPositions);
+    const newHeight = Math.abs(min - max);
+
+    /* 
+    Essentially we're looking for the cap height of the typeface, by looking for a change
+    in height over time. Eventually or stars will shift across the line (backwards),
+    which means we just passed the second at which the constellation appeared.
+    */
+   
+    if (newHeight < height) {
+      // When the height continues to shrink, we'll push the old set of instructions into the 'cache'
+      height = newHeight;
+      previousTicks.push(tick);
+      second++;
+    } else {
+      // With constellation time found, we'll pull the previous from our 'cache' of instructions
+      found = true;
+      second--;
+      tick = previousTicks[previousTicks.length - 1];
+    }    
+  }
+
+  const constellation = {
+    tick,
+    second
+  };
+
+  return constellation;
+};
 
 const findGridSize = input => {
   const positions = {
@@ -41,56 +94,6 @@ const buildGrid = ({minX, maxX, minY, maxY}) => {
   });
 };
 
-const findConstellation = data => {
-  let found = false;
-  let height = Infinity;
-  let second = 0;
-  let tick = data;
-  let previousTick;
-  
-  while (!found) {
-    second++;
-    tick = tick.map(({position, velocity}) => {
-      let x = position[0] += velocity[0];
-      let y = position[1] += velocity[1];
-      return {
-        position: [x, y],
-        velocity
-      };
-    });
-
-    const yPositions = tick.map(({position}) => position[1]);
-    const min = Math.min(...yPositions);
-    const max = Math.max(...yPositions);
-
-    let newHeight = Math.abs(min - max);
-
-    if (newHeight < height) {
-      height = newHeight;
-      // previousTick = tick;
-    } else {      
-      found = true;
-    }
-  }
-
-  // should cache the previous step?
-  tick = tick.map(({position, velocity}) => {
-    let x = position[0] -= velocity[0];
-    let y = position[1] -= velocity[1];
-    return {
-      position: [x, y],
-      velocity
-    };
-  });
-
-  const constellation = {
-    tick,
-    second: second - 1
-  };
-
-  return constellation;
-};
-
 const plotGrid = (points, grid, {minX, minY}) => {
   points.forEach((star) => {
     grid[star.position[1] - minY][star.position[0] - minX] = '#';
@@ -104,9 +107,18 @@ const createConstellation = data => {
   const instructions = findConstellation(data);
   const size = findGridSize(instructions.tick);
   const grid = buildGrid(size);
-  console.log(instructions);
+  console.log(`found at ${instructions.second}s!\n`);
   const constellation = flatten(plotGrid(instructions.tick, grid, size));
   return constellation;
 };
 
-console.log(createConstellation(data));
+// Why not, we're already being quite functional *shrug emoji*
+const print = constellation => console.log(constellation);
+
+const constellation = createConstellation(data);
+print(constellation);
+
+module.exports = {
+  print,
+  constellation
+};
